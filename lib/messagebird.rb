9fc8cb195d0144e284a05308fe682b631dcc5ca5
Bram de Vries <bram.devries@nedap.com>
2014-01-21 13:24:25 +0100

    Some refactoring

--- lib/messagebird.rb
+++ lib/messagebird.rb
@@ -10,50 +10,54 @@ module MessageBird
   class << self
 
     def send_text_message(phone_numbers, message)
-      load_variables
+      url = create_url(phone_numbers, message)
+      uri = create_uri(url)
 
-      connection(phone_numbers, message)
+      connection = create_connection(uri)
+
+      request = Net::HTTP::Get.new(uri.request_uri)
+      response = connection.request(request)
+      pp response.body
     end
 
     def configure(&block)
       MessageBird::Config.configure(&block)
     end
 
-  private
+protected
 
-    def load_variables
-      @username   = MessageBird::Config.username
-      @password   = MessageBird::Config.password
-      @api_url    = MessageBird::Config.api_url
-      @sender     = MessageBird::Config.sender_name
-      @test_mode  = MessageBird::Config.fetch(:test_mode, true) ? 1 : 0 # If test = 1, then the message is not actually sent or scheduled, and there will be no credits deducted
+    def create_connection(uri)
+      Net::HTTP.new(uri.host, uri.port).tap do |http|
+        http.use_ssl = true
+        # http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      end
     end
 
-    def connection(phone_numbers, message)
-      uri = URI.parse(request_url(phone_numbers, message))
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
-      # http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-      request = Net::HTTP::Get.new(uri.request_uri)
-      response = http.request(request)
-      pp response.body
+    def create_uri(url)
+      URI.parse(url)
     end
 
-    def request_url(phone_numbers, message)
+    def create_url(phone_numbers, message)
+      username    = MessageBird::Config.username
+      password    = URI.escape(MessageBird::Config.password)
+      api_url     = MessageBird::Config.api_url
+      sender      = MessageBird::Config.sender_name
+      test_mode   = MessageBird::Config.fetch(:test_mode, true) ? 1 : 0 # If test = 1, then the message is not actually sent or scheduled, and there will be no credits deducted
+
       destination = format_phone_numbers(phone_numbers)
-      message   = URI.escape(message)
-      @password = URI.escape(@password)
-      "#{@api_url}?username=#{@username}&password=#{@password}&sender=#{@sender}&test=#{@test_mode}&destination=#{destination}&body=#{message}"
+      message     = URI.escape(message)
+
+      "#{api_url}?username=#{username}&password=#{password}&sender=#{sender}&test=#{test_mode}&destination=#{destination}&body=#{message}"
     end
 
     def format_phone_numbers(phone_numbers)
+      # API accepts multiple phone numbers by seperating them with a comma ','
       if phone_numbers.is_a? Enumerable
-        # API accepts multiple phone numbers by seperating them with a comma ','
         phone_numbers.join(',')
       else
         "#{phone_numbers}"
       end
     end
+
   end
 end
--- test/messagebird_test.rb
+++ test/messagebird_test.rb
@@ -1,19 +1,40 @@
 require 'helper'
 require 'messagebird'
 
-describe MessageBird do
+module MessageBirdHelper
+  def format_phone_numbers(input)
+    subject.send :format_phone_numbers, input
+  end
+end
 
-  describe 'configuration' do
+describe MessageBird do
+  include MessageBirdHelper
+
+  subject{ MessageBird }
+  let(:config){ MessageBird::Config }
+
+  describe '#configure' do
     it 'it delegates configuration method calls to MessageBird::Config' do
-      mock(MessageBird::Config).configure.with_any_args.once
+      mock(config).configure.with_any_args.once
       MessageBird.configure do; end
     end
 
     it 'supports a block' do
-      MessageBird::Config.configure do
+      config.configure do
         api_url 'test'
       end
-      MessageBird::Config.api_url.must_equal 'test'
+      config.api_url.must_equal 'test'
+    end
+  end
+
+  describe '#format_phone_numbers' do
+    it 'converts an integer to string' do
+      format_phone_numbers(31541471696).must_equal "31541471696"
+    end
+
+    it 'converts an enumerable into a comma-separated string' do
+      numbers = [3154147100, "315472000"]
+      format_phone_numbers(numbers).must_equal "3154147100,315472000"
     end
   end
 
