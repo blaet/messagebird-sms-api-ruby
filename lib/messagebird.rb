f6cf133ece6641db047aa4ec285fc0a5e6d210f3
Bram de Vries <bram.devries@nedap.com>
2014-01-22 15:54:21 +0100

    Small requirement change

--- lib/messagebird.rb
+++ lib/messagebird.rb
@@ -1,5 +1,9 @@
 $LOAD_PATH.unshift File.dirname(__FILE__)
 
+require 'net/http'
+require 'net/https'
+require 'uri'
+
 require 'messagebird/config'
 require 'messagebird/deliverable'
 require 'messagebird/helpers'
@@ -8,13 +12,6 @@ require 'messagebird/http/sender'
 require 'messagebird/http/sms'
 require 'messagebird/http/response_code'
 require 'messagebird/http/response'
-
-
-require 'net/http'
-require 'net/https'
-require 'uri'
-
-require 'pp'
 
 module MessageBird
   class << self
